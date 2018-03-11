50156be1b6952c32c762d994fa25cdf64f70fc11
FrederikVanEecke <37119836+FrederikVanEecke@users.noreply.github.com>
2018-03-11 22:19:40 +0100

    kleine aanpassing.

--- src/File.java
+++ src/File.java
@@ -127,7 +127,7 @@ public class File {
 	
 	private Date creationTime = null;
 	private Date modificationTime = null; 
-	private String userPeriod; 
+	private long userPeriod; 
 	
 	
 	/**
@@ -150,8 +150,8 @@ public class File {
 		if (this.modificationTime == null) {
 			return 0;
 		}else {
-			long Diff = this.creationTime.getTime() - this.modificationTime.getTime(); 
-			return Diff; 
+			this.userPeriod = this.creationTime.getTime() - this.modificationTime.getTime(); 
+			return this.userPeriod; 
 		}
 	}
 	
